5448cc43a19be2d47fc4ea8e6a8560b6520d7fea
yohel715 <yohel715@gmail.com>
2020-06-17 20:28:44 -0600

    cambios menores al landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,35 +1,39 @@
 import React from "react";
+import Parallax from "react-rellax";
+
 import logo from "../img/ylogo_white.png";
 
 const Landing = () => {
   return (
     <div className="landing_content" id="landing">
-      <p className="landing-uptitle text-center m-3">
-        Desarrollador Web y Diseñador en Interfaz de Usuario
+      <p className="landing-uptitle text-center m-4">
+        Desarrollador Web y Diseñador de Interfaz de Usuario
       </p>
-      <h1 className="landing-title text-center m-3">Yohel Ureña Mora</h1>
+      <h1 className="landing_title text-center text-uppercase">Yohel Ureña Mora</h1>
+
       <div className="container landing__logo">
         <img
           src={logo}
-          height="440"
+          height="400"
           class="rounded mx-auto d-block mr-2"
           alt="logo"
         />
       </div>
-      <h6 className="landing-uptitle text-center m-3">
-        ¿Quieres saber más?
-      </h6>
-      <div className="icon d-flex justify-content-center">
-        <a className="icon_landing" href="#about">
-          <spam
-            className="iconify"
-            data-icon="bi:chevron-compact-down"
-            data-inline="false"
-            data-width="64"
-            data-height="64"
-          ></spam>
-        </a>
-      </div>
+
+      <Parallax speed={-1}>
+        <h6 className="landing-subtitle text-center m-3">¿Quieres saber más de mí?</h6>
+        <div className="icon d-flex justify-content-center">
+          <a className="icon_landing" href="#about">
+            <spam
+              className="iconify"
+              data-icon="bi:chevron-compact-down"
+              data-inline="false"
+              data-width="64"
+              data-height="64"
+            ></spam>
+          </a>
+        </div>
+      </Parallax>
     </div>
   );
 };
