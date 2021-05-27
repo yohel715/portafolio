3e396b3ddf96e4f89412956cbd606982e82078a3
yohel715 <yohel715@gmail.com>
2021-05-27 01:16:38 -0600

    new landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,39 +1,35 @@
 import React from "react";
-
-import logo from "../img/ylogo_white.png";
+import me from "../img/me.png";
 
 const Landing = () => {
   return (
-    <div className="landing_content" id="landing">
-      <p className="landing-uptitle text-center">
-        Desarrollador Web y Diseñador de Interfaz de Usuario
-      </p>
-      <h1 className="landing_title text-center text-uppercase">
-        Yohel Ureña Mora
-      </h1>
+    <div className="landing_content pt-5" id="landing">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-sm-6">
+             <div className="ml-5">
+                <h2 className="getting_up">Hi! I’m</h2>
+                <h1 className="landing_title">Yohel Ureña Mora</h1>
+                <h2 className="getting_down">
+                  Developer, UX, UI Designer <br/> & Graphic Designer
+                </h2>
+              </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="d-flex justify-content-center">  
+              <img
+                src={me}
+                height="400"
+                class="d-inline-block"
+                alt="me"
+              />
+            </div>
+            </div>
+          </div>
+        </div>
 
-      <div className="landing_logo">
-        <img
-          src={logo}  
-          class="landing_logo_img d-block"
-          alt="logo"
-        />
-      </div>
+        
 
-      <h6 className="landing-subtitle text-center m-3">
-        ¿Quieres saber más de mí?
-      </h6>
-      <div className="icon d-flex justify-content-center">
-        <a className="icon_landing" href="#about">
-          <spam
-            className="iconify"
-            data-icon="bi:chevron-compact-down"
-            data-inline="false"
-            data-width="64"
-            data-height="64"
-          ></spam>
-        </a>
-      </div>
     </div>
   );
 };
