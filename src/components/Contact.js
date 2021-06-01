5a34a0cacdd670030fa01bf8f05d551313197604
yohel715 <yohel715@gmail.com>
2021-06-01 01:13:06 -0600

    new contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import logo from "../img/SVG/logo.svg";
 
 const Contanct = () => {
   return (
@@ -6,7 +9,101 @@ const Contanct = () => {
       <h1>Contact Me</h1>
       <p>Let's create something new!</p>
 
-      <h2>Text me</h2>
+      <div className="row">
+        <div className="col-sm-7">
+          <h2>Text me</h2>
+
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" />
+              <Form.Text className="text-muted">
+                I'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>What's your name?</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Type here your message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Enter your message"
+              />
+            </Form.Group>
+            <Button variant="outline-primary  px-5 mb-4" type="submit">
+              Send
+            </Button>
+          </Form>
+        </div>
+        <div className="col-sm-5">
+          <div>
+            <h2>My social networks</h2>
+            <div className="d-flex py-2">
+              <span
+                class="iconify"
+                data-icon="dashicons:email-alt"
+                data-inline="false"
+                data-width="24"
+                data-height="24"
+              ></span>
+              <h5 className="font-weight-regular ml-2">yohel715@gmail.com</h5>
+            </div>
+            <div className="d-flex py-2">
+              <span
+                class="iconify"
+                data-icon="akar-icons:linkedin-fill"
+                data-inline="false"
+                data-width="24"
+                data-height="24"
+              ></span>
+              <h5 className="font-weight-regular ml-2">Yohel Ureña Mora</h5>
+            </div>
+            <div className="d-flex py-2">
+              <span
+                class="iconify"
+                data-icon="ant-design:instagram-filled"
+                data-inline="false"
+                data-width="24"
+                data-height="24"
+              ></span>
+              <h5 className="font-weight-regular ml-2">yohel.design</h5>
+            </div>
+            <div className="d-flex py-2">
+              <span
+                class="iconify"
+                data-icon="bi:phone-fill"
+                data-inline="false"
+                data-width="24"
+                data-height="24"
+              ></span>
+              <h5 className="font-weight-regular ml-2">+506 7013-6924</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <p>
+          © yohelurena.com <br /> Alajuela, Costa Rica <br /> 2021
+        </p>
+
+        <img src={logo} height="30" class="d-inline-block" alt="logo" />
+
+        <a href="#landing" className="contact_icon">
+          <span
+            class="iconify"
+            data-icon="bi:chevron-up"
+            data-inline="false"
+            data-width="24"
+            data-height="24"
+          ></span>
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import me from "../img/me.png";
+import Button from "react-bootstrap/Button";
 
 const Landing = () => {
   return (
@@ -7,24 +8,22 @@ const Landing = () => {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-sm-6">
-              <h2 className="getting_up">Hi! I’m</h2>
-              <h1 className="title">Yohel Ureña Mora</h1>
-              <h2 className="getting_down">
-                Developer, UX, UI Designer <br/> & Graphic Designer
-              </h2>
+            <h2 className="getting_up">Hi! I’m</h2>
+            <h1 className="title">Yohel Ureña Mora</h1>
+            <h2 className="getting_down">
+              Developer, UX, UI Designer <br /> & Graphic Designer
+            </h2>
           </div>
           <div className="col-sm-6">
-            <div className="d-flex justify-content-center">  
-              <img
-                src={me}
-                height="400"
-                class="d-inline-block"
-                alt="me"
-              />
-            </div>
+            <div className="d-flex justify-content-center mb-2">
+              <img src={me} height="400" class="d-inline-block" alt="me" />
             </div>
           </div>
         </div>
+        <Button variant="outline-primary" type="link" href="">
+          Check my curriculum
+        </Button>
+      </div>
     </div>
   );
 };
